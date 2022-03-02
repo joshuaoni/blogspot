00be48b8e83f085c431f6dfd886e29d5c15f3058
joshua <joshuadavidoni@yahoo.com>
2022-03-03 00:37:44 +0100

    c

--- src/Blogs.js
+++ src/Blogs.js
@@ -6,68 +6,68 @@ import BlogDetails from './BlogDetails';
 const Blogs = (props) => {  // props called title passed in to Blogs component
     const {title} = props;  // destructuring
 
-    const [blogs, deleteBlog, isPending, error] = BlogDetails();    //A custom hook
+    const [blogs, deleteBlog] = BlogDetails();    //A custom hook
 
     
-    // if (blogs === null) {
-    //     return (
-    //    <div>
-    //        <h1 id='heading'>{title}</h1>
-    //        <h1>Loading...</h1>
-    //    </div>          
-    //     );
-    // } else {
-    //     return (
-    //         <div>
-    //             <h1 id='heading'>{title}</h1>
-    //             {blogs.map((blog, i)=>{
-    //                 return (
-    //                     <div key={blog.id}>
-    //                         <div id='img'className='blogText'>
-    //                             <Link to={`${i}`}>
-    //                                 <div>
-    //                                     <h1>Title: <span>{blog.title}</span></h1>
-    //                                     <h1>Author: <span>{blog.author}</span></h1>
-    //                                 </div>
-    //                             </Link> 
-    //                             <button className='btn' onClick={()=>deleteBlog(blog.id)}>Delete</button>                       
-    //                         </div>
-    //                         <br></br>
-    //                     </div>
-    //                 );
-    //             })}
-    //         </div>
-    //     );
-    // }
-    
-
-    return (
-        <div>
-            <h1 id='heading'>{title}</h1>
-
-            {error && <h1>error 419 : {error} :(</h1>}
-
-            {isPending && <h1>Loading...</h1>}
-
-            {blogs && blogs.map((blog, i)=>{    // the && operator : if the left is false, it doesn't run what is on the right of the operator. However, if the left is true, it moves over to the right of the operator and runs 
-                return (
-                    <div key={blog.id}>
-                        <div id='img'className='blogText'>
-                            <Link to={`${i}`}>
-                                <div>
-                                    <h1>Title: <span>{blog.title}</span></h1>
-                                    <h1>Author: <span>{blog.author}</span></h1>
-                                </div>
-                            </Link> 
-                            <button className='btn' onClick={()=>deleteBlog(blog.id)}>Delete</button>                       
+    if (blogs === null) {
+        return (
+       <div>
+           <h1 id='heading'>{title}</h1>
+           <h1>Loading...</h1>
+       </div>          
+        );
+    } else {
+        return (
+            <div>
+                <h1 id='heading'>{title}</h1>
+                {blogs.map((blog, i)=>{
+                    return (
+                        <div key={blog.id}>
+                            <div id='img'className='blogText'>
+                                <Link to={`${i}`}>
+                                    <div>
+                                        <h1>Title: <span>{blog.title}</span></h1>
+                                        <h1>Author: <span>{blog.author}</span></h1>
+                                    </div>
+                                </Link> 
+                                <button className='btn' onClick={()=>deleteBlog(blog.id)}>Delete</button>                       
+                            </div>
+                            <br></br>
                         </div>
-                        <br></br>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
+        );
+    }
+    
 
-        </div>
-    );
+    // return (
+    //     <div>
+    //         <h1 id='heading'>{title}</h1>
+
+    //         {error && <h1>error 419 : {error} :(</h1>}
+
+    //         {isPending && <h1>Loading...</h1>}
+
+    //         {blogs && blogs.map((blog, i)=>{    // the && operator : if the left is false, it doesn't run what is on the right of the operator. However, if the left is true, it moves over to the right of the operator and runs 
+    //             return (
+    //                 <div key={blog.id}>
+    //                     <div id='img'className='blogText'>
+    //                         <Link to={`${i}`}>
+    //                             <div>
+    //                                 <h1>Title: <span>{blog.title}</span></h1>
+    //                                 <h1>Author: <span>{blog.author}</span></h1>
+    //                             </div>
+    //                         </Link> 
+    //                         <button className='btn' onClick={()=>deleteBlog(blog.id)}>Delete</button>                       
+    //                     </div>
+    //                     <br></br>
+    //                 </div>
+    //             );
+    //         })}
+
+    //     </div>
+    // );
     
 }
  
--- src/WriteUp.js
+++ src/WriteUp.js
@@ -7,18 +7,15 @@ const WriteUp = () => {
     const {i} = useParams();
     const customHookReturns = BlogDetails();    //A custom hook
     const blogs = customHookReturns[0]  
-    const isPending = customHookReturns[2]
-    const error = customHookReturns[3]
     
 
     return (
         <div>
-            {error && <h1>error 22 : {error} :(</h1>}
-            {isPending && <h1>Loading...</h1>}
+            
 
-            {blogs && <h1 id='heading'>{blogs[i].title}</h1>}
+            <h1 id='heading'>{blogs[i].title}</h1>
 
-            {blogs && <><h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -28,9 +25,9 @@ const WriteUp = () => {
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></h1></>}
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></h1>
             
-            {blogs && <h1 className='flex'><p className='padl'>Written by {blogs[i].author}.</p></h1>}
+            <h1 className='flex'><p className='padl'>Written by {blogs[i].author}.</p></h1>
         </div>
     );
 }
